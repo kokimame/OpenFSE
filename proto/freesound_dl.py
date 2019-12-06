425aa969c1e6f0ab113aba09ab41a532e558d24f
kokimame <kohkimametani@gmai.com>
2019-12-06 15:41:11 +0100

    Improve download script

--- proto/freesound_dl.py
+++ proto/freesound_dl.py
@@ -5,13 +5,17 @@ import freesound
 client = freesound.FreesoundClient()
 client.set_token("3uYSYQldiW7rON8ksZNTrUPNrs7SV8MRGUDYCDRd", "token")
 
-results = client.text_search(query="106737",fields="id,previews")
-
-print(results)
-
 with open("json/ground_truth_annotations_28_05_19.json", "r") as f:
     annons = json.load(f)
-    print(len(annons))
 
-for sound in results:
-    sound.retrieve_preview("DLSOUND", f"{sound.id}.mp3")
+idset = []
+for i, (id, label, _, duration) in enumerate(annons):
+    if 5 < duration <= 20 and id not in idset:
+        idset.append(id)
+    if len(idset) >= 10:
+        break
+
+for id in idset:
+    sound = client.get_sound(id, fields="id,previews")
+
+    sound.retrieve_preview(f"{os.environ['HOME']}/Project/DLSOUND", f"{sound.id}.mp3")
