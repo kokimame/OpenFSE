1ce002d6d112ab88a5caf40c7fa921826c3011c2
kokimame <kohkimametani@gmai.com>
2019-12-12 02:45:31 +0100

    Chunk sound files to make triplets

--- proto/freesound_dl.py
+++ proto/freesound_dl.py
@@ -4,6 +4,9 @@ import freesound
 
 client = freesound.FreesoundClient()
 client.set_token("3uYSYQldiW7rON8ksZNTrUPNrs7SV8MRGUDYCDRd", "token")
+DLTOP = f"{os.environ['HOME']}/Project/Master_Files/audio"
+if not os.path.exists(DLTOP):
+    os.makedirs(DLTOP)
 
 with open("json/ground_truth_annotations_28_05_19.json", "r") as f:
     annons = json.load(f)
@@ -12,10 +15,10 @@ idset = []
 for i, (id, label, _, duration) in enumerate(annons):
     if 5 < duration <= 20 and id not in idset:
         idset.append(id)
-    if len(idset) >= 10:
+    if len(idset) >= 100:
         break
 
 for id in idset:
     sound = client.get_sound(id, fields="id,previews")
-
-    sound.retrieve_preview(f"{os.environ['HOME']}/Project/DLSOUND", f"{sound.id}.mp3")
+    # This is not efficient. Use a filter
+    sound.retrieve_preview(DLTOP, f"{sound.id}.mp3")
