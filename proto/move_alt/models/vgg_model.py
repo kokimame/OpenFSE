e09b7e30e7fb0961dc4f72765f19d07923c316a4
kokimame <code.kohkimametani@gmail.com>
2020-05-05 00:31:18 +0200

    Zero padding

--- proto/move_alt/models/vgg_model.py
+++ proto/move_alt/models/vgg_model.py
@@ -15,37 +15,50 @@ class VGGModel(nn.Module):
         self.prelu3 = nn.PReLU(init=0.01)
         self.prelu4 = nn.PReLU(init=0.01)
         self.prelu5 = nn.PReLU(init=0.01)
+        self.prelu6 = nn.PReLU(init=0.01)
 
         self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
 
         self.conv1 = nn.Conv2d(in_channels=1,
                                out_channels=16,
-                               kernel_size=(3, 3))
+                               kernel_size=(3, 3),
+                               padding=1)
         self.conv2 = nn.Conv2d(in_channels=16,
                                out_channels=16,
-                               kernel_size=(3, 3))
+                               kernel_size=(3, 3),
+                               padding=1)
         self.conv3 = nn.Conv2d(in_channels=16,
                                out_channels=16,
-                               kernel_size=(3, 3))
+                               kernel_size=(3, 3),
+                               padding=1)
         self.conv4 = nn.Conv2d(in_channels=16,
                                out_channels=16,
-                               kernel_size=(3, 3))
+                               kernel_size=(3, 3),
+                               padding=1)
         self.conv5 = nn.Conv2d(in_channels=16,
                                out_channels=16,
-                               kernel_size=(3, 3))
+                               kernel_size=(3,3),
+                               padding=1)
+        # 5 conv without padding is possible too!
+        self.conv6 = nn.Conv2d(in_channels=16,
+                               out_channels=16,
+                               kernel_size=(3,3),
+                               padding=1)
         self.lin1 = nn.Linear(in_features=64, out_features=emb_size, bias=True)
+        self.lin_bn = nn.BatchNorm1d(emb_size, affine=False)
 
         self.fin_emb_size = emb_size
         self.double()
 
     def forward(self, data):
-        x = self.prelu1(self.maxpool(self.conv1(data)))
-        x = self.prelu2(self.maxpool(self.conv2(x)))
-        x = self.prelu3(self.maxpool(self.conv3(x)))
-        x = self.prelu4(self.maxpool(self.conv4(x)))
-        x = self.prelu5(self.maxpool(self.conv5(x)))
+        x = self.prelu1(self.maxpool(self.conv1(data))) # 64x64x16
+        x = self.prelu2(self.maxpool(self.conv2(x))) # 32x32x16
+        x = self.prelu3(self.maxpool(self.conv3(x))) # 16x16x16
+        x = self.prelu4(self.maxpool(self.conv4(x))) # 8x8x16
+        x = self.prelu5(self.maxpool(self.conv5(x))) # 4x4x16
+        x = self.prelu6(self.maxpool(self.conv6(x))) # 2x2x16
 
         x = torch.flatten(x, start_dim=1)
         x = self.lin1(x)
-        x = torch.sigmoid(x)
+        x = self.lin_bn(x)
         return x
