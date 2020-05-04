bbc72b3b8679a27de611f2a6dcf1e4ba117aac10
kokimame <code.kohkimametani@gmail.com>
2020-05-05 00:32:24 +0200

    Update spectrogram with normalization (v2)

--- proto/generate_spec_v2.py
+++ proto/generate_spec_v2.py
@@ -11,28 +11,42 @@ from mutagen.mp3 import MP3
 
 # Suppress soundfile warning on loading MP3
 warnings.simplefilter('ignore')
-
+DATA_MAX = 10000
 DOWNLOAD_DIR = f'{os.environ["HOME"]}/Project/Master_Files/audio_tagged'
-SPEC_DIR = f'{os.environ["HOME"]}/Project/Master_Files/spec_tagged'
+SPEC_DIR = f'{os.environ["HOME"]}/Project/Master_Files/spec_tagged_v2'
 
 # Check if the directories exist
 if not os.path.exists(SPEC_DIR):
     os.makedirs(SPEC_DIR)
 
+SAMPLING_FREQUENCY = 16000
+NFTT = 1024
+HOP_LENGTH = 512
 CHUNK_PER_SOUND = 5
 CHUNK_WIDTH = 128
 
-def compute_spectrogram(path, sr=16000, n_mels=128):
+def get_melspec(spec, n_mels):
+    # Power spectrum
+    powerspec = np.abs(spec)**2
+    melspec = librosa.feature.melspectrogram(S=powerspec, n_mels=n_mels)
+    S = librosa.power_to_db(melspec, np.max)
+    return S
+
+def get_spectrum(path, n_fft, hop_length, sr=None):
     try:
-        audio, sr = librosa.load(path, sr=sr)
+        y, sr = librosa.load(path, sr=sr)
     except:
-        audio, o_sr = sf.read(path)
-        audio = librosa.core.resample(audio, o_sr, sr)
-    audio_rep = librosa.feature.melspectrogram(
-        y=audio, sr=sr, hop_length=512, n_fft=1024, n_mels=n_mels, power=1.
-    )
-    audio_rep = np.log(audio_rep + np.finfo(np.float32).eps)
-    return audio_rep
+        y, o_sr = sf.read(path)
+        y = librosa.core.resample(y, o_sr, sr)
+    y_stft = librosa.core.stft(y, n_fft=n_fft, hop_length=hop_length)
+    return y_stft
+
+def compute_spectrogram(path, sr=SAMPLING_FREQUENCY, n_mels=128):
+    y_stft = get_spectrum(path, n_fft=NFTT, sr=sr, hop_length=HOP_LENGTH)
+    melspec = get_melspec(y_stft, n_mels=n_mels)
+    # Convert melspec from -80 to 0dB to range [0,1]
+    spec = (melspec + 80.0) / 80.0
+    return spec
 
 def pad_2d_horizontal(a2d, target_length):
     h, w = a2d.shape
@@ -62,10 +76,17 @@ def save_chucks(chunks, original_path):
         chunk_path = f'{SPEC_DIR}/{label}/{existing + i + 1:06d}.npy'
         np.save(chunk_path, chunk)
 
+def visualize_chunks(path, chunks):
+    for i, chunk in enumerate(chunks):
+        plt.subplot(2, 3, i + 1)
+        plt.imshow(chunk)
+    plt.xlabel(path)
+    plt.show()
+
 audio_paths = glob.glob(f'{DOWNLOAD_DIR}/*/*.mp3')
 
-for path in tqdm(audio_paths[:10000]):
-    if MP3(path).info.length <= 12:
+for path in tqdm(audio_paths[:DATA_MAX]):
+    if MP3(path).info.length <= 15:
         continue
     spec = compute_spectrogram(path)
     chunks = spec_to_chunk(spec)
