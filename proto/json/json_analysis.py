d54e79ff3d9771c659de49e2f208d66b703365f5
kokimame <code.kohkimametani@gmail.com>
2020-05-07 11:50:41 +0200

    Count unique labels

--- proto/json/json_analysis.py
+++ proto/json/json_analysis.py
@@ -12,11 +12,12 @@ for sound, label, _, _ in data:
     labels.append(label)
     lookup[sound] = labels
 
+unique_sounds = set([d[0] for d in data])
 vals = [len(v) for k, v in lookup.items()]
 
 plt.hist(vals)
 plt.xticks(range(0, max(vals)))
 plt.ylabel('# of sounds')
 plt.xlabel('# of labels assigned to a sound')
-plt.title(f'FS AudioSet annotation data. Size: {len(data)}')
+plt.title(f'Unique Sound Size: {len(unique_sounds)}')
 plt.show()
