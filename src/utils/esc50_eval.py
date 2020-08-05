ca447ae9f97914426d3df89d255cf3536a2fa6ca
kokimame <code.kohkimametani@gmail.com>
2020-08-05 11:17:10 +0200

    Completed evaluation script: Discriminative tasks and clustering tasks

--- src/utils/esc50_eval.py
+++ src/utils/esc50_eval.py
@@ -6,12 +6,14 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-
-from torch.utils.data import Dataset
 from tqdm import tqdm, trange
+from torch.utils.data import Dataset
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import adjusted_rand_score
+
 from src.utils.utils import import_dataset_from_pt
 from models.model_vgg_dropout import VGGModelDropout
-from sklearn.metrics import accuracy_score
 
 
 class Classifier(nn.Module):
@@ -64,7 +66,7 @@ class ESC50Dataset(Dataset):
             openfse.load_state_dict(torch.load(f'{model_path}'))
             self.data = openfse(torch.cat(data).unsqueeze(dim=1)).detach()
         else:
-            self.data = [torch.squeeze(d) for d in data]
+            self.data = torch.stack([torch.squeeze(d) for d in data])
         labels_list = list(set(labels))
         self.labels = torch.LongTensor([
             labels_list.index(label) for label in labels
@@ -76,51 +78,6 @@ class ESC50Dataset(Dataset):
     def __getitem__(self, index):
         return self.data[index], self.labels[index]
 
-
-# -------------- Specifying paths and loading datasets ----------------
-
-# MODEL_PATH = '../saved_models/multi_top100_ma_8k.pt'
-MODEL_MA_PATH = '../saved_models/model_multi_top500_2020-07-31_10:58:32.673148.pt'
-MODEL_WOMA_PATH = '../saved_models/model_multi_top500_2020-07-31_10:58:32.673148.pt'
-ESC50_DIR = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50'
-ESC50_SPEC = f'{ESC50_DIR}/spec'
-
-TRAIN_SPEC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_all_train_1.pt'
-VAL_SPEC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_all_val.pt'
-TRAIN_MFCC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_mfcc_train_1.pt'
-VAL_MFCC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_mfcc_val.pt'
-
-train_spec_data, train_spec_labels, _ = import_dataset_from_pt('{}'.format(TRAIN_SPEC_PATH), chunks=1)
-val_spec_data, val_spec_labels, _ = import_dataset_from_pt('{}'.format(VAL_SPEC_PATH), chunks=1)
-
-train_ma_set = ESC50Dataset(train_spec_data, train_spec_labels, model_path=MODEL_MA_PATH)
-train_ma_loader = torch.utils.data.DataLoader(train_ma_set, batch_size=4, shuffle=True)
-print(f'Train (MA) data has been loaded! | Data Size: {len(train_spec_data)}')
-val_ma_set = ESC50Dataset(val_spec_data, val_spec_labels, model_path=MODEL_MA_PATH)
-val_ma_loader = torch.utils.data.DataLoader(val_ma_set, batch_size=len(val_ma_set), shuffle=True)
-print(f'Validation (MA) data has been loaded! | Data Size: {len(val_spec_data)}')
-
-train_woma_set = ESC50Dataset(train_spec_data, train_spec_labels, model_path=MODEL_WOMA_PATH)
-train_woma_loader = torch.utils.data.DataLoader(train_woma_set, batch_size=4, shuffle=True)
-print(f'Train (WOMA) data has been loaded! | Data Size: {len(train_spec_data)}')
-val_woma_set = ESC50Dataset(val_spec_data, val_spec_labels, model_path=MODEL_WOMA_PATH)
-val_woma_loader = torch.utils.data.DataLoader(val_woma_set, batch_size=len(val_woma_set), shuffle=True)
-print(f'Validation (WOMA) data has been loaded! | Data Size: {len(val_spec_data)}')
-
-train_mfcc_data, train_mfcc_labels, _ = import_dataset_from_pt('{}'.format(TRAIN_MFCC_PATH), chunks=1)
-train_mfcc_set = ESC50Dataset(train_mfcc_data, train_mfcc_labels)
-train_mfcc_loader = torch.utils.data.DataLoader(train_mfcc_set, batch_size=4, shuffle=True)
-print(f'Train (MFCC) data has been loaded! | Data Size: {len(train_mfcc_data)}')
-
-val_mfcc_data, val_mfcc_labels, _ = import_dataset_from_pt('{}'.format(VAL_MFCC_PATH), chunks=1)
-val_mfcc_set = ESC50Dataset(val_mfcc_data, val_mfcc_labels)
-val_mfcc_loader = torch.utils.data.DataLoader(val_mfcc_set, batch_size=len(val_mfcc_set), shuffle=True)
-print(f'Validation (MFCC) data has been loaded! | Data Size: {len(val_mfcc_data)}')
-print()
-time.sleep(1)
-
-# -----------------------------------------------------------------------
-# ------------------- Start training -------------------------
 def train(classifier, train_loader, val_loader):
     train_loss, val_loss = [], []
     train_xticks, val_xticks = [], []
@@ -162,19 +119,25 @@ def train(classifier, train_loader, val_loader):
 
     return train_loss, val_loss, train_xticks, val_xticks, val_acc
 
-# ------------------- Plotting figures ---------------------------------
-def plot_figrues():
-    classifier_emb = Classifier(input_shape=EMBEDDING_SHAPE, version='plain')
-    classifier_mfcc = Classifier(input_shape=MFCC_SHAPE, version='plain')
+def discriminative_tasks(classifier_version):
+    # --------------------------------------------------------------------
+    # Classification on ESC50 dataset using different types of input data
+    # --------------------------------------------------------------------
+    # Classifier for embedding input (OpenFSE)
+    classifier_emb = Classifier(input_shape=EMBEDDING_SHAPE, version=classifier_version)
+    # Classifier for MFCC input
+    classifier_mfcc = Classifier(input_shape=MFCC_SHAPE, version=classifier_version)
 
     result_lookup = {'loss' : {}, 'acc' : {}}
     for classifier, train_loader, val_loader, setting_name in [
-        (classifier_mfcc, train_mfcc_loader, val_mfcc_loader, 'MFCC'),
-        (classifier_emb, train_ma_loader, val_ma_loader, 'MA'),
-        (classifier_emb, train_woma_loader, val_woma_loader, 'WOMA'),
+        (classifier_mfcc, train_mfcc_loader, val_mfcc_loader, 'MFCC'),  # Baseline
+        (classifier_emb, train_ma_loader, val_ma_loader, 'MA'),         # With margin adapter
+        (classifier_emb, train_woma_loader, val_woma_loader, 'WOMA'),   # Without margin adapter
     ]:
+        print(' -------------------------------- ')
+        print(f'Start: Training classifier for {setting_name}')
+        print(' -------------------------------- \n')
         time.sleep(1)
-        print(f'Start training for {setting_name}')
         tl, vl, txs, vxs, va = train(classifier, train_loader, val_loader)
         result_lookup['loss'][setting_name] = [tl, txs, vl, vxs]
         result_lookup['acc'][setting_name] = [vxs, va]
@@ -200,8 +163,76 @@ def plot_figrues():
 
     plt.show()
 
+
+def clustering_tasks():
+    print(' ----------------------------------------------- ')
+    print('Start: Clustering tasks using Adjusted Rand Score')
+    print(' ----------------------------------------------- \n')
+
+    result_lookup = {}
+    for train_set, val_set, setting_name in [
+        (train_mfcc_set, val_mfcc_set, 'MFCC'),  # Baseline
+        (train_ma_set, val_ma_set, 'MA'),         # With margin adapter
+        (train_woma_set, val_woma_set, 'WOMA'),   # Without margin adapter
+    ]:
+        kmeans = KMeans(n_clusters=50, random_state=0).fit(torch.cat([train_set.data, val_set.data]))
+        ars = adjusted_rand_score(kmeans.labels_, torch.cat([train_set.labels, val_set.labels]))
+        result_lookup[setting_name] = ars
+        print(f'Adjusted Rand Score ({setting_name}): {ars}')
+
+    plt.bar(np.arange(len(result_lookup)), result_lookup.values(), align='center', alpha=0.5)
+    plt.xticks(np.arange(len(result_lookup)), result_lookup.keys())
+    plt.ylabel('Adjusted Rand Score')
+    plt.show()
+
+
 EMBEDDING_SHAPE = 512
 MFCC_SHAPE = 120
 NUMBER_OF_EPOCHS = 300
+CLASSIFIER_VERSION = 'plain'
+# -------------- Specifying paths and loading datasets ----------------
+MODEL_MA_PATH = '../saved_models/multi_top100_ma_8k.pt'
+MODEL_WOMA_PATH = '../saved_models/multi_top100_woma_8k.pt'
+ESC50_DIR = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50'
+ESC50_SPEC = f'{ESC50_DIR}/spec'
 
-plot_figrues()
+TRAIN_SPEC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_all_train_1.pt'
+VAL_SPEC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_all_val.pt'
+TRAIN_MFCC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_mfcc_train_1.pt'
+VAL_MFCC_PATH = '/media/kokimame/Work_A_1TB/Project/Master_Files/ESC-50/esc50_mfcc_val.pt'
+
+print(' ---------------------------- ')
+print('Start: Loading models and data')
+print(' ---------------------------- \n')
+train_spec_data, train_spec_labels, _ = import_dataset_from_pt('{}'.format(TRAIN_SPEC_PATH), chunks=1)
+val_spec_data, val_spec_labels, _ = import_dataset_from_pt('{}'.format(VAL_SPEC_PATH), chunks=1)
+
+train_ma_set = ESC50Dataset(train_spec_data, train_spec_labels, model_path=MODEL_MA_PATH)
+train_ma_loader = torch.utils.data.DataLoader(train_ma_set, batch_size=4, shuffle=True)
+print(f'Train (MA) data has been loaded!\t\t\t | Data Size: {len(train_spec_data)}')
+val_ma_set = ESC50Dataset(val_spec_data, val_spec_labels, model_path=MODEL_MA_PATH)
+val_ma_loader = torch.utils.data.DataLoader(val_ma_set, batch_size=len(val_ma_set), shuffle=True)
+print(f'Validation (MA) data has been loaded!\t\t | Data Size: {len(val_spec_data)}')
+
+train_woma_set = ESC50Dataset(train_spec_data, train_spec_labels, model_path=MODEL_WOMA_PATH)
+train_woma_loader = torch.utils.data.DataLoader(train_woma_set, batch_size=4, shuffle=True)
+print(f'Train (WOMA) data has been loaded!\t\t\t | Data Size: {len(train_spec_data)}')
+val_woma_set = ESC50Dataset(val_spec_data, val_spec_labels, model_path=MODEL_WOMA_PATH)
+val_woma_loader = torch.utils.data.DataLoader(val_woma_set, batch_size=len(val_woma_set), shuffle=True)
+print(f'Validation (WOMA) data has been loaded!\t\t | Data Size: {len(val_spec_data)}')
+
+train_mfcc_data, train_mfcc_labels, _ = import_dataset_from_pt('{}'.format(TRAIN_MFCC_PATH), chunks=1)
+train_mfcc_set = ESC50Dataset(train_mfcc_data, train_mfcc_labels)
+train_mfcc_loader = torch.utils.data.DataLoader(train_mfcc_set, batch_size=4, shuffle=True)
+print(f'Train (MFCC) data has been loaded!\t\t\t | Data Size: {len(train_mfcc_data)}')
+
+val_mfcc_data, val_mfcc_labels, _ = import_dataset_from_pt('{}'.format(VAL_MFCC_PATH), chunks=1)
+val_mfcc_set = ESC50Dataset(val_mfcc_data, val_mfcc_labels)
+val_mfcc_loader = torch.utils.data.DataLoader(val_mfcc_set, batch_size=len(val_mfcc_set), shuffle=True)
+print(f'Validation (MFCC) data has been loaded!\t\t | Data Size: {len(val_mfcc_data)}')
+print()
+time.sleep(1)
+
+
+clustering_tasks()
+discriminative_tasks(CLASSIFIER_VERSION)
